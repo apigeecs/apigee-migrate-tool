a64fddebf7e92dfb4b1c6e7efc8c4f87fb5f02a2
Kurt Kanaskie <kurtkanaskie@google.com>
2019-08-19 13:11:38 -0400

    Anonomized

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ module.exports = {
 	from: {
 		version: '19.04',
 		url: 'https://api.enterprise.apigee.com',
-		userid: 'kurtkanaskie@google.com',
-		passwd: 'Rocky20!9',
-		org: 'kurtkanaskietrainer-trial',
+		userid: 'admin@google.com',
+		passwd: 'SuperSecret123',
+		org: 'org1',
 		env: 'test'
 	},
 	to: {
 		version: '19.04',
 		url: 'https://api.enterprise.apigee.com',
-		userid: 'kurtkanaskie@google.com',
-		passwd: 'Rocky20!9',
-		org: 'kurtkanaskie',
+		userid: 'admin@google.com',
+		passwd: 'SuperSecret123!9',
+		org: 'org2',
 		env: 'test'
 	}
 } ;
