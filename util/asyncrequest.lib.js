6ce33bf75f7b05a622a06aa1d7c232e8f470741f
Adam Jones <adam.j.jones@cdk.com>
2021-04-19 14:46:41 +0100

    Allow aync requests with no callback

--- util/asyncrequest.lib.js
+++ util/asyncrequest.lib.js
@@ -5,40 +5,48 @@ const request = require('request');
 module.exports = function (grunt, userid, passwd) {
 	let pending_tasks = 0;
 
-	const waitForGet = function (url, callback) {
+	const waitForGet = function (url, callback = null) {
 		++pending_tasks;
 		let r = request(url, function (error, response, body) {
-			callback(error, response, body);
+			if (callback)
+				callback(error, response, body);
+
 			--pending_tasks;
 		}).auth(userid, passwd, true);
 
 		return r;
 	}
 
-	const waitForPost = function (url, opts, callback) {
+	const waitForPost = function (url, opts, callback = null) {
 		++pending_tasks;
 		let r = request.post({ url: url, ...opts }, function (error, response, body) {
-			callback(error, response, body);
+			if (callback)
+				callback(error, response, body);
+
 			--pending_tasks;
 		}).auth(userid, passwd, true);
 
 		return r;
 	}
 
-	const waitForPut = function (url, opts, callback) {
+	const waitForPut = function (url, opts, callback = null) {
 		++pending_tasks;
 		let r = request.put({ url: url, ...opts }, function (error, response, body) {
-			callback(error, response, body);
+			if (callback)
+				callback(error, response, body);
+
 			--pending_tasks;
 		}).auth(userid, passwd, true);
 
 		return r;
 	}
 
-	const waitForDelete = function (url, callback) {
+	const waitForDelete = function (url, callback = null) {
 		++pending_tasks;
 		let r = request.del(url, function (error, response, body) {
-			callback(error, response, body);
+			if (callback)
+				callback(error, response, body);
+
 			--pending_tasks;
 		}).auth(userid, passwd, true);
 
@@ -48,10 +56,9 @@ module.exports = function (grunt, userid, passwd) {
 	const waitForCompletion = function (donecb = null) {
 		let intervalId = setInterval(function () {
 			if (pending_tasks <= 0) {
-				if (donecb) {
+				if (donecb)
 					donecb();
-				}
-	
+
 				clearInterval(intervalId);
 			}
 			else {
